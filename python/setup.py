888f52c512375a5c9b9bfaf3df3ae3ac1b5413e1
tim <tim@elec.ac.nz>
2021-05-13 10:57:56 +1200

    More code cleanup

--- python/setup.py
+++ python/setup.py
@@ -6,29 +6,32 @@ from setuptools import setup, find_packages
 
 import setuptools.command.test
 
-with open('README.md') as f:
+with open("README.md") as f:
     readme = f.read()
 
-setup(name='fastfix',
-    version='0.1.0b2',
-    description='FastFix Positioning',
+setup(
+    name="fastfix",
+    version="0.1.0b2",
+    description="FastFix Positioning",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url='http://github.com/elec-otago/projects/fastfix',
-    author='Tim Molteno',
-    test_suite='nose.collector',
-    tests_require=['nose', 'ephem'],
-    author_email='tim@elec.ac.nz',
-    license='GPLv3',
-    install_requires=['numpy', 'matplotlib', 'scipy', 'pyfftw', 'unlzw', 'pymc3'],
-    packages=['fastfix'],
-    scripts=['bin/fastfix', 'bin/acquire'],
+    url="http://github.com/elec-otago/projects/fastfix",
+    author="Tim Molteno",
+    test_suite="nose.collector",
+    tests_require=["nose", "ephem"],
+    author_email="tim@elec.ac.nz",
+    license="GPLv3",
+    install_requires=["numpy", "matplotlib", "scipy", "pyfftw", "unlzw", "pymc3"],
+    packages=["fastfix"],
+    scripts=["bin/fastfix", "bin/acquire"],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Topic :: Scientific/Engineering",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        "Intended Audience :: Science/Research"])
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Intended Audience :: Science/Research",
+    ],
+)
