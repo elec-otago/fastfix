3d714de2aeddf3ef17f18f17d5ee8e45e0e6d873
tim <tim@elec.ac.nz>
2021-05-14 14:01:05 +1200

    Added slight testbench for gaussian llh

--- python/fastfix/tests/test_util.py
+++ python/fastfix/tests/test_util.py
@@ -11,5 +11,5 @@ class TestUtil(unittest.TestCase):
     def test_gaussian_llh(self):
         
         self.assertAlmostEqual(Util.gaussian_llh(0,0,2), -1.612, 3)
-        self.assertAlmostEqual(Util.gaussian_llh(0,0,1), -0.918, 3)
+        self.assertAlmostEqual(Util.gaussian_llh(0,0,1), -0.919, 3)
 
