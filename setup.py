035e13ef1d8d2ac83d6d1862ba1e7a4e09566be5
Tim Molteno <tim@4i.nz>
2023-08-02 22:16:58 +1200

    Use correct encoding for opening the README. Closes #7.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 import setuptools.command.test
 
-with open("README.md") as f:
+with open("README.md", 'r', encoding="utf-8") as f:
     readme = f.read()
 
 setup(
